38d32b94a459010b1b18248f8ff257f86097efe4
Ropkoy <valchebett@gmail.com>
2024-03-28 14:36:51 +0300

    Data Types Assignment

---
+++ Week1.dart
@@ -0,0 +1,46 @@
+//void main() {
+//declaring variable
+//String name = "Valentine";
+//String address = "Lower Kabete, Macai Road";
+//num age = 27;
+//num height = 5.8;
+//bool isMarried = false;
+
+//print variables
+// print("Name is $name");
+//print("Address is $address");
+//print("Age is $age");
+//print("Height is $height");
+//print("Married Status is $isMarried");
+//}
+//void main() {
+//declaring variables
+//int num1 = 200;
+// double num2 = 19.7;
+//num num3 = 59;
+//num num4 = 23.4;
+
+//for sum
+//num sum = num1 + num2 + num3 + num4;
+
+//printing info
+// print("Num 1 is $num1");
+// print("Num 2 is $num2");
+// print("Num 3 is $num3");
+// print("Num 4 is $num4");
+// print("Sum is $sum");
+//}
+//void main() {
+//declaring variables
+//String schoolName = "Power Learn Project";
+//String address = "Spring Valley, Lower Kabete Road Kenya,Africa";
+//Printing values
+//print("My school name is $schoolName and the address is $address");
+//}
+//Functions
+void printName() {
+  print ("My name is Valentine");
+}
+void main() {
+  printName();
+}
